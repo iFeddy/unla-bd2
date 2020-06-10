fb36042b5221165501de1695420915a90db74add
Feddy <feddy_7990@hotmail.com>
2020-06-10 18:38:30 -0300

    Agregado Ventas / Inventario Class

---
+++ FarmaciaDB/src/Farmacia/Inventario.java
@@ -0,0 +1,69 @@
+package Farmacia;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Inventario {
+
+    @SerializedName("id")
+    @Expose private String id;
+    @SerializedName("tipo")
+    @Expose private Integer tipo;
+    @SerializedName("desc")
+    @Expose private String desc;
+    @SerializedName("laboratorio")
+    @Expose private String laboratorio;
+    @SerializedName("codnum")
+    @Expose private Integer codnum;
+    @SerializedName("precio")
+    @Expose private String precio;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Integer tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getLaboratorio() {
+        return laboratorio;
+    }
+
+    public void setLaboratorio(String laboratorio) {
+        this.laboratorio = laboratorio;
+    }
+
+    public Integer getCodnum() {
+        return codnum;
+    }
+
+    public void setCodnum(Integer codnum) {
+        this.codnum = codnum;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+}
--- FarmaciaDB/src/Farmacia/Ventas.java
+++ FarmaciaDB/src/Farmacia/Ventas.java
@@ -1,5 +1,77 @@
 package Farmacia;
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 public class Ventas {
-    //Para hacer!?
+    @SerializedName("id")
+    @Expose private String id;
+    @SerializedName("ticket")
+    @Expose private Integer ticket;
+    @SerializedName("total")
+    @Expose private String total;
+    @SerializedName("formapago")
+    @Expose private Integer formapago;
+    @SerializedName("vendedor")
+    @Expose private String vendedor;
+    @SerializedName("cajero")
+    @Expose private String cajero;
+    @SerializedName("inventario")
+    @Expose private List<Inventario> inventario = null;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(Integer ticket) {
+        this.ticket = ticket;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public Integer getFormapago() {
+        return formapago;
+    }
+
+    public void setFormapago(Integer formapago) {
+        this.formapago = formapago;
+    }
+
+    public String getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(String vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public String getCajero() {
+        return cajero;
+    }
+
+    public void setCajero(String cajero) {
+        this.cajero = cajero;
+    }
+
+    public List<Inventario> getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(List<Inventario> inventario) {
+        this.inventario = inventario;
+    }
 }
